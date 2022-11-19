408cec7f1fc73c2f3f1b7dff0d6c34bae6495400
Anshul Kanwar <4n5hu1@protonmail.com>
2022-11-19 13:13:16 +0530

    Initial Commit

---
+++ Khaane mai kya hai/Khaane_mai_kya_haiApp.swift
@@ -0,0 +1,17 @@
+//
+//  Khaane_mai_kya_haiApp.swift
+//  Khaane mai kya hai
+//
+//  Created by Anshul Kanwar on 19/11/22.
+//
+
+import SwiftUI
+
+@main
+struct Khaane_mai_kya_haiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
